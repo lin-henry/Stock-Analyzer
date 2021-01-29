2c65fa19103a9106b88a43e0c0127e99cdb483cc
Henry Lin <linhenry001@gmail.com>
2021-01-28 19:56:03 -0500

    update code

--- stockanalyzer.py
+++ stockanalyzer.py
@@ -1,4 +1,15 @@
 import pandas as pd
+from bs4 import BeautifulSoup as bs
+import requests
 
-ticker = ['AAPL','MSFT']
+
+# Creating user ticker input
+ticker_input = str(input("Enter a ticker: "))
+print("Retrieving financial data for: {}".format(ticker_input))
+
+# Scraping webpage
+url = 'https://finviz.com/quote.ashx?t={}'.format(ticker_input.upper())
+webpage_request = requests.get(url)
+webpage_content = webpage_request.content
+webpage_source = bs(webpage_content, 'lxml')
 
