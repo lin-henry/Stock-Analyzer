72d0bc3827399f57f1ce6f5dea34505597613d88
Henry Lin <linhenry001@gmail.com>
2021-01-29 20:43:06 -0500

    Code changes

--- stockanalyzer.py
+++ stockanalyzer.py
@@ -1,15 +1,22 @@
 import pandas as pd
+import numpy as np
 from bs4 import BeautifulSoup as bs
-import requests
+from urllib.request import Request, urlopen
 
-
-# Creating user ticker input
+# Creating ticker url
 ticker_input = str(input("Enter a ticker: "))
 print("Retrieving financial data for: {}".format(ticker_input))
+url = 'https://finviz.com/quote.ashx?t={}'.format(ticker_input.upper())
 
 # Scraping webpage
-url = 'https://finviz.com/quote.ashx?t={}'.format(ticker_input.upper())
-webpage_request = requests.get(url)
-webpage_content = webpage_request.content
-webpage_source = bs(webpage_content, 'lxml')
+def web_scraper(url):
+    request = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(request).read()
+    html_source = str(bs(webpage, "html.parser"))
+    return html_source
 
+web_scraper(url)
+
+# Data cleaning financial summary table from Finviz
+
+financial_summary = pd.read_html(html_source, attrs = {'class': 'snapshot-table2'}[0]
