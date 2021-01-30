af2f1dedde474124e1ae15bc0b44c2a47ee1efb8
Henry Lin <linhenry001@gmail.com>
2021-01-29 21:34:38 -0500

    Added multiple ticker summary

--- stockanalyzer.py
+++ stockanalyzer.py
@@ -1,27 +1,31 @@
 import pandas as pd
-import numpy as np
 from bs4 import BeautifulSoup as bs
 from urllib.request import Request, urlopen
 
-# Creating ticker url
-ticker_input = str(input("Enter a ticker: "))
-print("Retrieving financial data for: {}".format(ticker_input))
-url = 'https://finviz.com/quote.ashx?t={}'.format(ticker_input.upper())
-
-# Scraping webpage
+# Web scraping function
 def web_scraper(url):
     request = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(request).read()
     html_source = str(bs(webpage, "html.parser"))
     return html_source
 
-html_source = web_scraper(url)
+# Creating ticker urls
+ticker_input = str(input("Enter a ticker(s) seperate ticker by spaces: ")).upper()
+ticker_list = [ticker for ticker in ticker_input.split()]
+print("Retrieving financial data for: {}".format(ticker_input + "\n"))
+
+for tickers in ticker_list:
+    url = 'https://finviz.com/quote.ashx?t=' + tickers
+    html_source = web_scraper(url)
+
 
 # Data cleaning financial summary table from Finviz for multiple ticker comparison
 financial_df = pd.read_html(html_source, attrs = {'class': 'snapshot-table2'})[0]
-
 length_columns = len(financial_df.columns.tolist())
 
+df_list = []
 for split_times in range(2,length_columns +2 ,2):
-    split_df = financial_df.iloc[:,split_times - 2:split_times]
-    
+    split_df = financial_df.iloc[:,split_times - 2:split_times].set_index(split_times - 2)
+    df_list.append(split_df)
+
+ticker_summary = pd.DataFrame()
