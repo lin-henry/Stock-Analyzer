ee2516d42a126d572bc44ac2e78700214e0bfd0c
Henry Lin <linhenry001@gmail.com>
2021-01-31 23:20:14 -0500

    Code updates

--- stockanalyzer.py
+++ stockanalyzer.py
@@ -43,17 +43,16 @@ tickers_df = pd.concat(tickers_df_list, axis = 1)
 
 # Removing unwanted rows for plotting
 
-drop_unwanted_rows = tickers_df.drop('Index
-'Employees'
-Optionable
-Shortable
-Insider Own
-Insider Trans
-RSI(14)
-Rel Volume
-Avg Volume
-Volume
-ATR
-Beta
-Volatility
-)
+drop_unwanted_rows = tickers_df.drop(['Index',
+'Employees',
+'Optionable',
+'Shortable',
+'Insider Own',
+'Insider Trans',
+'RSI(14)',
+'Rel Volume',
+'Avg Volume',
+'Volume',
+'ATR',
+'Beta',
+'Volatility'])
